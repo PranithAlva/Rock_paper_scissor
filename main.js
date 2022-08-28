553b59ea08c3409f63f12c12a1758df59a428e6b
Pranith P Alva <alvapranith@gmail.com>
2022-08-28 19:28:43 +0530

    Add javascript and develop an user interface for the game
    Here we use javascript to manipulate the DOM and add an user interface to interact with the user
    to take the input from the user and play the game.

--- main.js
+++ main.js
@@ -1,13 +1,60 @@
-/* playRound function*/
-function playRound(playerSelection, computerSelection) {
-	if (playerSelection == computerSelection) return [`It is a Tie`, 0, 0];
-	if (
-		(playerSelection == "rock" && computerSelection == "scissor") ||
-		(playerSelection == "paper" && computerSelection == "rock") ||
-		(playerSelection == "scissor" && computerSelection == "paper")
-	)
-		return [`You Win! ${playerSelection} beats ${computerSelection}`, 1, 0];
-	return [`You Lose! ${computerSelection} beats ${playerSelection}`, 0, 1];
+/*
+author:Pranith
+ */
+
+//reset button
+function resetStatus() {
+	document.getElementById("userScoreBox").innerText = 0;
+	document.getElementById("computerScoreBox").innerText = 0;
+	let winnerBox = document.querySelector("#board");
+	let reset = document.getElementById("reset");
+	winnerBox.removeChild(reset);
+	let matchOutcome = document.getElementById("match-outcomes");
+	matchOutcome.textContent = "";
+	let result = document.querySelector("#result");
+	result.textContent = null;
+	winnerBox.textContent = null;
+}
+
+//display reset button
+function addResetButton() {
+	const winnerBoard = document.querySelector("#board");
+	const reset = document.createElement("button");
+	reset.setAttribute("id", "reset");
+	reset.setAttribute(
+		"style",
+		"background:rgb(0, 162, 255);color:white;height:50px;"
+	);
+	reset.textContent = "Reset";
+	winnerBoard.appendChild(reset);
+	reset.addEventListener("click", () => {
+		resetStatus();
+	});
+}
+
+//function to check who has reached five points
+function checkWinner() {
+	userScore = Number(document.getElementById("userScoreBox").textContent);
+	computerScore = Number(
+		document.getElementById("computerScoreBox").textContent
+	);
+	if (userScore == 5) {
+		document.getElementById("winner-board").innerText = "You win!!!";
+		addResetButton();
+	}
+	if (computerScore == 5) {
+		document.getElementById("winner-board").innerText = "You Loose!!! :-(";
+		addResetButton();
+	}
+}
+
+//display result in html dox
+function displayResult(verdict) {
+	const matchOutcome = document.querySelector("#match-outcomes");
+	const div = document.createElement("p");
+	div.setAttribute("id", "inetermediate-result");
+	div.textContent = verdict;
+	matchOutcome.appendChild(div);
 }
 
 /*get computer choice */
@@ -20,38 +67,41 @@ function getcomputerChoice() {
 	return computerSelection;
 }
 
-/*game function */
-function game() {
-	let userScore = 0;
-	let computerScore = 0;
-	for (let i = 0; i < 5; i++) {
-		/*capture user input */
-		let playerSelection = prompt("Rock/Paper/Scissor");
-		/*adjust the input to be case-insensitive */
-		playerSelection = playerSelection.toLowerCase();
-		/*capture computer input*/
-		const computerSelection = getcomputerChoice();
-		result = playRound(playerSelection, computerSelection);
-		console.log(result[0]);
-		computerScore += result[2];
-		userScore += result[1];
+//main compute function
+function playRound(playerSelection) {
+	const computerSelection = getcomputerChoice();
+	document.getElementById(
+		"result"
+	).innerText = `Computer chooses ${computerSelection}`;
+	if (playerSelection == computerSelection) {
+		displayResult(`It is a tie`);
+		return;
 	}
-	/*Decide the winner*/
-	let decision = "You Lose!!";
-	if (computerScore == userScore) {
-		decision = "It is a tie!!";
-		console.log(decision);
-	} else if (userScore > computerScore) {
-		decision = "You Win!!";
-		console.log(decision);
-	} else {
-		console.log(decision);
-	}
+	if (
+		(playerSelection == "rock" && computerSelection == "scissor") ||
+		(playerSelection == "paper" && computerSelection == "rock") ||
+		(playerSelection == "scissor" && computerSelection == "paper")
+	) {
+		let text = parseInt(document.getElementById("userScoreBox").textContent);
+		document.getElementById("userScoreBox").innerText = ++text;
+		displayResult(`You Win! ${playerSelection} beats ${computerSelection}`);
 
-	/*write scores into html*/
-	document.getElementById("userScoreBox").innerText = userScore;
-	document.getElementById("computerScoreBox").innerText = computerScore;
-	document.getElementById("result").innerText = decision;
+		return;
+	}
+	let text = parseInt(document.getElementById("computerScoreBox").textContent);
+	document.getElementById("computerScoreBox").innerText = ++text;
+	displayResult(`You Lose! ${computerSelection} beats ${playerSelection}`);
 }
-/*function call*/
-game();
+
+/*game function */
+function game(keys) {
+	playRound(keys);
+	checkWinner();
+}
+
+const keys = document.querySelectorAll("button");
+keys.forEach((keys) => {
+	keys.addEventListener("click", () => {
+		game(keys.id);
+	});
+});
